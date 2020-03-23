aa5335f137e3d6f343c47611c777b6fa033fd6d9
Brandon Yoon <byoon5397@gmail.com>
2020-03-23 19:52:17 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,34 +1,42 @@
 def join_ingredients(src)
   newArr = []
-  # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
-  #
-  # "I love (inner array element 0) and (inner array element 1) on my pizza""
-  # As such, there should be a new String for each inner array, or pair
   row_index = 0
   while row_index < src.count do
     element_index = 0
       while element_index < src[row_index].count do
-        newArr << "I love " src[row_index] "and" src[row_index][element_index] " on my pizza"
+        src[row_index].each do |ele1| && src[row_index][element_index] do |ele2|
+        newArr << "I love " ele1 " and " ele2 " on my pizza"
   end
 
   return newArr
 end
 
 def find_greater_pair(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # Produce a new Array that contains the larger number of each of the pairs
-  # that are in the inner Arrays
+
 end
 
 def total_even_pairs(src)
-  # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
-  # if both numbers in the pair are even, then add both those numbers to the
-  # total
-  #
-  # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
-  # the number was even. Review the operator documentation if you've forgotten
-  # this!
+
 end
+
+# Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
+#
+# Build a new Array that contains strings where each pair of foods is
+# inserted into this template:
+#
+# "I love (inner array element 0) and (inner array element 1) on my pizza""
+# As such, there should be a new String for each inner array, or pair
+
+
+# src will be an array of [ [number1, number2], ... [numberN, numberM] ]
+# Produce a new Array that contains the larger number of each of the pairs
+# that are in the inner Arrays
+
+
+# src will be an array of [ [number1, number2], ... [numberN, numberM] ]
+# if both numbers in the pair are even, then add both those numbers to the
+# total
+#
+# As a reminder any number % 2 will return 0 or 1. If the result is 0, then
+# the number was even. Review the operator documentation if you've forgotten
+# this!
