77f4cd60a4fccca8bc3baaea42f277608d823f1b
Brandon Yoon <byoon5397@gmail.com>
2020-03-23 19:53:21 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,11 +1,5 @@
 def join_ingredients(src)
-  newArr = []
-  row_index = 0
-  while row_index < src.count do
-    element_index = 0
-      while element_index < src[row_index].count do
-        src[row_index].each do |ele1| && src[row_index][element_index] do |ele2|
-        newArr << "I love " ele1 " and " ele2 " on my pizza"
+  return src
   end
 
   return newArr
